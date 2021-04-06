9f2fd9e29f5235155a989c13d095640e3d2909ef
chenjian <chenjian0112@made-in-china.com>
2021-04-06 13:07:50 +0800

    custom

--- src/pages/StockEvent/index.jsx
+++ src/pages/StockEvent/index.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+import {Card, Button, Space} from 'antd'
 // import events from './events'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { connect, history } from 'umi';
-
+import 'moment/locale/zh-cn';
+// 将日历选择为国际化
+moment.locale('zh-cn');
 const localizer = momentLocalizer(moment)
 
 @connect(({ stock, loading }) => ({
@@ -47,37 +50,57 @@ export default class Page extends Component {
     })
   }
 
+  custom_event = ({ event }) => {
+    return (
+      <span>
+        <strong>{event.title}</strong>
+        {event.ts_code && `【${event.ts_code}】`}
+        {event.desc && ':  ' + event.desc}
+      </span>
+      )
+  }
+
   render(){
     const {events} = this.state;
+    const {loading} = this.props
     const CalendarToolbar = ({ onView, label, views }) => (
-      <div>
-        <h2>
-          {label}
-        </h2>
-        {views.map(view => (
-          <button
-            key={view}
-            type="button"
-            onClick={() => onView(view)}
-          >
-            月
-          </button>
-        ))}
-      </div>
+      <Card
+        bordered={false}
+        title={
+          <h2>{label}</h2>
+        }
+        extra={
+          <Space>
+            {views.map(view => (
+              <Button
+                size="large"
+                key={view}
+                onClick={() => onView(view)}
+              >
+                {view==='month' && '月度'}
+                {view==='agenda' && '议程'}
+              </Button>
+            ))}
+          </Space>
+        }
+      />
     );
     return(
-      <div>
+      <Card loading={loading}>
         <Calendar
           localizer={localizer}
           events={events}
-          // components={{
-          //   toolbar: CalendarToolbar,
-          // }}
+          components={{
+            event: this.custom_event,
+            toolbar: CalendarToolbar,
+          }}
+          views={['month','agenda']}
           startAccessor="start"
           endAccessor="end"
           style={{height: '100vh'}}
+          onSelectSlot={e=>console.log(e)}
         />
-      </div>
+      </Card>
     )
   }
 }
